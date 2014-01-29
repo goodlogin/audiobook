f22526f6a411425347fea8952cdab07fca52a967
Andrew <andrew@sorochak.com>
2014-01-29 16:43:53 +0200

    init commit

---
+++ AudioBook.WP8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using AudioBook.WP8.Resources;
+
+namespace AudioBook.WP8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
